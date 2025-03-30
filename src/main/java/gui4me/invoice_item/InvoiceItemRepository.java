32d06f5998239b7aa771190095d12e1f9d4218e4
tiago ertel <erteltiago0@gmail.com>
2025-03-30 00:07:49 +0000

    list invoices and items

--- src/main/java/gui4me/invoice_item/InvoiceItemRepository.java
+++ src/main/java/gui4me/invoice_item/InvoiceItemRepository.java
@@ -1,8 +1,13 @@
 package gui4me.invoice_item;
 
+import gui4me.invoice.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, String> {
+
+    List<InvoiceItem> findAllByInvoiceId(String invoiceId);
 }
