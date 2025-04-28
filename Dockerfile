f7fdaa8aee993488cc777d0903cb30ecffb41495
tiago ertel <erteltiago0@gmail.com>
2025-04-28 22:18:56 +0100

    configuring to run with docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Stage 1: Build the application
-FROM maven:3.9-eclipse-temurin-21 AS build
+FROM maven:3.9-eclipse-temurin-23 AS build
 WORKDIR /app
 COPY . .
 RUN mvn clean package -DskipTests
--- src/main/java/gui4me/Application.java
+++ src/main/java/gui4me/Application.java
@@ -2,6 +2,7 @@ package gui4me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 public class Application {
