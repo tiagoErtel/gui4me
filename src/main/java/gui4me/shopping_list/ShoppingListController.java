9e652ff99cc8129d8f5cf066e685a666515e937d
tiago ertel <erteltiago0@gmail.com>
2025-06-25 15:26:21 +0100

    use currentUser model attribute

--- src/main/java/gui4me/shopping_list/ShoppingListController.java
+++ src/main/java/gui4me/shopping_list/ShoppingListController.java
@@ -1,16 +1,20 @@
 package gui4me.shopping_list;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import gui4me.user.User;
 import gui4me.utils.Message;
 import gui4me.utils.MessageType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/shopping-list")
@@ -20,23 +24,21 @@ public class ShoppingListController {
     ShoppingListService shoppingListService;
 
     @GetMapping
-    public String list(Model model, Authentication auth) {
-        if (auth.getPrincipal() instanceof User user) {
-            List<ShoppingList> lists = shoppingListService.findByUser(user);
-            model.addAttribute("shoppingLists", lists);
-        }
+    public String list(Model model, @ModelAttribute("currentUser") User user) {
+
+        List<ShoppingList> lists = shoppingListService.findByUser(user);
+        model.addAttribute("shoppingLists", lists);
         return "pages/shopping_list/list";
     }
 
     @PostMapping("/create")
     public String create(
             @RequestParam String name,
-            Authentication auth,
+            @ModelAttribute("currentUser") User user,
             RedirectAttributes redirectAttributes) {
-        if (auth.getPrincipal() instanceof User user) {
-            shoppingListService.createShoppingList(name, user);
-            redirectAttributes.addFlashAttribute("message", new Message(MessageType.SUCCESS, "List created!"));
-        }
+
+        shoppingListService.createShoppingList(name, user);
+        redirectAttributes.addFlashAttribute("message", new Message(MessageType.SUCCESS, "List created!"));
         return "redirect:/shopping-list";
     }
 
