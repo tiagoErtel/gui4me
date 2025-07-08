fc9efe8929b37d5f1d1a75c4ffcf408eab4e703f
tiago ertel <erteltiago0@gmail.com>
2025-07-08 10:17:51 +0100

    improve exception message

--- src/main/java/gui4me/exceptions/user/UserVerificationTokenExpiredException.java
+++ src/main/java/gui4me/exceptions/user/UserVerificationTokenExpiredException.java
@@ -3,7 +3,7 @@ package gui4me.exceptions.user;
 public class UserVerificationTokenExpiredException extends RuntimeException {
 
     public UserVerificationTokenExpiredException() {
-        super("User verification token has expired");
+        super("Token expired");
     }
 
 }
